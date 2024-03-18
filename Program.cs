6ba851dd10433c38cc6f41b4fef06e228b92630b
s19568 <s19568@pjwstk.edu.pl>
2024-03-18 15:37:31 +0100

    Commit Zadanie 4 rebase

--- Program.cs
+++ Program.cs
@@ -2,13 +2,11 @@
 
 public class Program
 {
-    public static double CalculateAverageFromIntegerArray(int [] num)
+    public static double FindAverage(int [] num)
     {
         int avg = 0;
         if (num == null || num.Length == 0)
-        {
             throw new ArgumentException("You provided an empty array or null array");
-        }
 
         for (int i = 0; i < num.Length; i++)
         {
@@ -17,9 +15,25 @@ public class Program
 
         return (double) avg /num.Length;
     }
+
+    public static int FindMax(int[] num)
+    {
+        int max = 0;
+        if (num == null || num.Length == 0)
+            throw new ArgumentException("You provided an empty array or null array");
+        for (int i = 0; i < num.Length; i++)
+        {
+            if (num[i] > max)
+                max = num[i];
+        }
+        return max;
+    }
     public static void Main(string[] args)
     {
         int[] numbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-        Console.WriteLine("Average: " + CalculateAverageFromIntegerArray(numbers));
+        int[] numbersForMax = { 2, 3, 5, 7, 11, 29, 19, 23, 17 };
+        Console.WriteLine("Average: " + FindAverage(numbers));
+        Console.WriteLine("Max number: " + FindMax(numbersForMax));
     }
 }
+
