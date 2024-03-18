e9019ddb520b119d7d8370a195d4a7ac7d8858a1
s19568 <s19568@pjwstk.edu.pl>
2024-03-18 15:10:53 +0100

    Zadanie 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static double CalculateAverageFromIntegerArray(int [] num)
+    {
+        int avg = 0;
+        if (num == null || num.Length == 0)
+        {
+            throw new ArgumentException("You provided an empty array or null array");
+        }
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            avg += num[i];
+        }
+
+        return (double) avg /num.Length;
+    }
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+        Console.WriteLine("Average: " + CalculateAverageFromIntegerArray(numbers));
+    }
+}
